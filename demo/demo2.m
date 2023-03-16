07d8cbea4301a27e5f73e507077853f27697c78f
djdesant <djdesant@mail.ru>
2023-03-16 20:30:41 +0100

    Encapsulated files in a Matlab project Added model files for Matlab R2022b

---
+++ demo/demo2.m
@@ -0,0 +1,68 @@
+clear
+close all
+clc
+
+addpath('../models/proprietary/R2022b/FM')
+addpath('..')
+
+smallDrivelineSimScape_oscSpring_flex_config
+
+model_file_name = 'smallDrivelineSimScape_oscSpring_flex';
+open_system(model_file_name);
+
+%% Run conversion script
+
+rtwbuild(model_file_name)
+%%
+out = system(['python ../run.py ' model_file_name]);
+assert(out==0);
+
+rehash
+
+
+%%
+
+addpath('/home/jgillis/programs/casadi/matlab_install/casadi')
+model = Model;
+
+model.input_names
+
+%%
+model.variable_names
+%%
+model.parameter_names
+%%
+model.nx
+model.np
+%%
+
+import casadi.*
+
+x = SX.sym('x',model.nx);
+z = SX.sym('z',model.nz);
+u = SX.sym('u',model.nu);
+p = SX.sym('p',model.np);
+t = SX.sym('t');
+
+dae = model.dae_expl;
+
+[ode, alg] = dae(x,z,u,p,t,[],[],[]);
+
+size(ode)
+size(alg)
+
+%%
+ode
+alg
+%%
+jacobian(ode,[x;z])
+
+%%
+jacobian(alg,z)
+celldisp(symvar(ans))
+%%
+model.variable_names{4}
+%%
+zsol = -jacobian(alg,z)\substitute(alg,z,0)
+size(zsol)
+
